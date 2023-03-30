e26b0ec2364e4777064c5dc7c30eaf273adfb177
beto <emely.rodrigues@estudante.ifms.edu.br>
2023-03-30 18:57:05 -0400

    30 MAR

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HersComponent } from './components/hers/hers.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HersComponent
+    HersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
