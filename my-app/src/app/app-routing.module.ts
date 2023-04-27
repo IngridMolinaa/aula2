992a6678704a939d4d184344b1e220083e02d92c
beto <emely.rodrigues@estudante.ifms.edu.br>
2023-04-27 15:01:03 -0400

    7777

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,14 +4,16 @@ import { HersComponent } from './components/hers/hers.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { LoginComponent } from './components/login/login.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: 'heroes', component: HersComponent},
   {path: 'login', component: LoginComponent},
   {path: 'create', component: CreateUserComponent},
+  {path: 'dashboard', component: DashboardComponent},
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent},
-  
+
 ];
 
 @NgModule({
